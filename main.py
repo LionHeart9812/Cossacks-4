e9902d6a4dfb8c45e62e99e86c1686398ecda6c8
LionHeart <leolev.nabok@gmail.com>
2026-02-15 20:10:37 +0100

    Cossack4 v0.4

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import pygame as pg
+import time
+import random
 
 pg.init()
+
+# --- Global stuff --- #
+gameRunning = True
+currentScreen = "start"
+i = 1
+cossackX, cossackY = 1000, 400
+
+# Timer(s)
+ONE_SECOND_EVENT = pg.USEREVENT + 1
+pg.time.set_timer(ONE_SECOND_EVENT, 200)
 
 # --- Colors, fonts and images, etc. --- #
 info = pg.display.Info()
 WIDTH = info.current_w
 HEIGHT = info.current_h
-print(WIDTH, HEIGHT)
+print(WIDTH, "x", HEIGHT, "Pixel")
 
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Cossacks 4")
@@ -20,8 +32,11 @@ buttonFont = pg.font.Font("assets/fonts/UncialAntiqua-Regular.ttf", 35)
 
 background = pg.image.load("assets/img/background.png").convert_alpha()
 
-# --- General stuff --- #
+# Hoomans
+cossack = pg.image.load("assets/img/cossack.png").convert_alpha()
+cossack = pg.transform.scale(cossack, (64, 64))
 
+# --- General stuff --- #
 def refresh():
     pg.display.flip()
 
@@ -45,50 +60,83 @@ def Button(X, Y, WIDTH, HEIGHT, colorR, colorG, colorB, text, font, textCOL, Rad
     pg.draw.rect(screen, (colorR, colorG, colorB), middleRect, 0, Radius)
 
     Text(text, font, textCOL, X, Y, True)
-    refresh()
 
     return borderRect, middleRect
 
-# Global variables
-gameRunning = True
+def generateNewDirection():
+    minDirectionX  = random.randrange(-15, -6)
+    minDirectionY = random.randrange(-2, -1)
+    maxDirectionX  = random.randrange(6, 10)
+    maxDirectionY = random.randrange(1, 2)
+    return minDirectionX, minDirectionY, maxDirectionX, maxDirectionY
 
 
-# --- Drawing Variabels --- #
-def drawStart():
+# --- Drawing Functions --- #
+def drawStart(x, y):
     screen.blit(background, (0,0))
     Text("COSSACKS 4", startMenuFont, BLACK, WIDTH / 2, 250, True)
     start, startBorder = Button(WIDTH / 2, HEIGHT / 2 + 20, 250, 90, 150, 150, 150, "START", buttonFont, BLACK, 10)
     settings, settingsBorder = Button(WIDTH / 2, HEIGHT / 2 + 200, 250, 90, 150, 150, 150, "SETTINGS", buttonFont, BLACK, 10)
-    refresh()
+
+    screen.blit(cossack, (x, y))
 
     return start, startBorder, settings, settingsBorder
 
 def drawGame():
     screen.fill(WHITE)
     Text("Dis is game", startMenuFont, BLACK, WIDTH / 2, HEIGHT / 2, True)
-    refresh()
 
 def drawSettings():
     screen.fill(WHITE)
     Text("Dis is settings", startMenuFont, BLACK, WIDTH / 2, HEIGHT / 2, True)
-    refresh()
 
 # ----------- Game ----------- #
-start, startBorder, settings, settingsBorder = drawStart()
+start, startBorder, settings, settingsBorder = drawStart(cossackX, cossackY)
+minDirectionX, minDirectionY, maxDirectionX, maxDirectionY = generateNewDirection()
 
-while gameRunning == True:
+while gameRunning:
+    # --- Event Handling ---
     for event in pg.event.get():
-        mousePOS = pg.mouse.get_pos()
-
         if event.type == pg.QUIT:
             gameRunning = False
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            mousePOS = pg.mouse.get_pos()
+            if currentScreen == "start":
+                if start.collidepoint(mousePOS) or startBorder.collidepoint(mousePOS):
+                    currentScreen = "game"
+                    drawGame()
+                elif settings.collidepoint(mousePOS) or settingsBorder.collidepoint(mousePOS):
+                    currentScreen = "settings"
+                    drawSettings()
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_ESCAPE and currentScreen == "settings":
+                currentScreen = "start"
 
-        if event.type == pg.MOUSEBUTTONDOWN and (start.collidepoint(mousePOS) or startBorder.collidepoint(mousePOS)):
-            drawGame()
+        # --- Cossack Logic --- #
+        elif currentScreen == "start" and event.type == ONE_SECOND_EVENT:
+            cossackX += random.randrange(minDirectionX, maxDirectionX)
+            cossackY += random.randrange(minDirectionY, maxDirectionY)
 
-        if event.type == pg.MOUSEBUTTONDOWN and (settings.collidepoint(mousePOS) or settingsBorder.collidepoint(mousePOS)):
-            drawSettings()
+            if cossackX <= 0 or cossackX >= 1400:
+                cossackX = random.randrange(0, 1400)
+                cossackY= random.randrange(0, 800)
 
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_ESCAPE:
-                drawStart()
+                minDirectionX, minDirectionY, maxDirectionX, maxDirectionY = generateNewDirection()
+
+            if cossackY <= 0 or cossackY >= 1400:
+                cossackX = random.randrange(0, 1400)
+                cossackY= random.randrange(0, 800)
+
+                minDirectionX, minDirectionY, maxDirectionX, maxDirectionY = generateNewDirection()
+
+    # --- Draw --- #
+    screen.fill((0,0,0))
+    if currentScreen == "start":
+        drawStart(cossackX, cossackY)
+    elif currentScreen == "settings":
+        drawSettings()
+    elif currentScreen == "game":
+        drawGame()
+
+    refresh()
+    pg.time.Clock().tick(24)  # ~24 FPS
